2edd8a5773d3616abaa9555652ab1bbeb429afc9
= <=>
2024-08-01 12:55:48 +0530

    Complete challange to add copyright text to the website.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const name = "Arihant Jain";
-const luckNumber = Math.ceil(Math.random() * 9);
+const currentYear = new Date().getFullYear();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>Hello {name}, How are you!</h1>
-    <p>Your Luck Number is: {luckNumber}</p>
+    <p>Created by {name}</p>
+    <p>Copyright {currentYear}</p>
   </div>
 );

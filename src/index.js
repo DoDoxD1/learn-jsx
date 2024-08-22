ac2b4a910fde758935cb23cbef9e3590f5357dde
= <=>
2024-08-22 10:26:43 +0530

    es6 exports and imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { add, multiply, subtract, divide } from "./calculator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>
+);
